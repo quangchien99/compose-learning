a46334c2cbe3d4592b4337f8f3872a5c548f407d
Chien <chien.pham@onemount.com>
2023-07-29 09:41:13 +0700

    inventory: create database

---
+++ InventoryApp/app/src/main/java/om/platform/inventoryapplication/data/InventoryDatabase.kt
@@ -0,0 +1,31 @@
+package om.platform.inventoryapplication.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+
+        fun getDatabase(context: Context): InventoryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    /**
+                     * Setting this option in your app's database builder means that Room
+                     * permanently deletes all data from the tables in your database when it
+                     * attempts to perform a migration with no defined migration path.
+                     */
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
---
+++ InventoryApp/app/src/main/java/om/platform/inventoryapplication/data/Item.kt
@@ -0,0 +1,16 @@
+package om.platform.inventoryapplication.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entity data class represents a single row in the database.
+ */
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val price: Double,
+    val quantity: Int
+)

b08b0856b31a7ea40c9380d6dd6f7d37f1485d86
Chien <chien.pham@onemount.com>
2023-07-29 09:44:04 +0700

    inventory: add offline repository

---
+++ InventoryApp/app/src/main/java/om/platform/inventoryapplication/data/OfflineItemsRepository.kt
@@ -0,0 +1,25 @@
+package om.platform.inventoryapplication.data
+
+import kotlinx.coroutines.flow.Flow
+
+class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
+    override fun getAllItemsStream(): Flow<List<Item>> {
+        return itemDao.getAllItems()
+    }
+
+    override fun getItemStream(id: Int): Flow<Item?> {
+        return itemDao.getItem(id)
+    }
+
+    override suspend fun insertItem(item: Item) {
+        itemDao.insert(item)
+    }
+
+    override suspend fun deleteItem(item: Item) {
+        itemDao.delete(item)
+    }
+
+    override suspend fun updateItem(item: Item) {
+        itemDao.update(item)
+    }
+}

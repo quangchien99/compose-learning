e9b0f14f52402818950a6b5aca1d3397626d9f19
Chien <chien.pham@onemount.com>
2023-07-29 09:26:12 +0700

    inventory: add necessary resources

--- InventoryApp/app/src/main/java/om/platform/inventoryapplication/ui/theme/Shape.kt
+++ InventoryApp/app/src/main/java/om/platform/inventoryapplication/ui/theme/Shape.kt
@@ -1,11 +1,11 @@
 package om.platform.inventoryapplication.ui.theme
 
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    extraSmall = CutCornerShape(topEnd = 8.dp, bottomStart = 8.dp),
+    small = CutCornerShape(topEnd = 8.dp, bottomStart = 8.dp),
+    medium = CutCornerShape(topEnd = 16.dp, bottomStart = 16.dp)
 )

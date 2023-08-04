c634232bc3b8cf9b44b65c4e70f24317dc65c741
Chien <chien.pham@onemount.com>
2023-08-04 16:41:54 +0700

    cashly: update theming

--- Cashly/app/src/main/java/chn/phm/cashly/ui/theme/CashlyTheme.kt
+++ Cashly/app/src/main/java/chn/phm/cashly/ui/theme/CashlyTheme.kt
@@ -4,6 +4,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Green500,
@@ -13,10 +17,41 @@ private val DarkColorScheme = darkColorScheme(
     onBackground = Color.White
 )
 
+private val DarkDialogColorScheme = darkColorScheme(
+    primary = Color.White,
+    surface = Color.White.copy(alpha = 0.12f).compositeOver(Color.Black),
+    onSurface = Color.White
+)
+
 /**
- * A [MaterialTheme] for Rally.
+ * A [MaterialTheme] for Cashly.
  */
 @Composable
 fun CashlyTheme(content: @Composable () -> Unit) {
     MaterialTheme(colorScheme = DarkColorScheme, typography = Typography, content = content)
+}
+
+/**
+ * A theme overlay used for dialogs.
+ */
+@Composable
+fun CashlyDialogThemeOverlay(content: @Composable () -> Unit) {
+    val currentTypography = MaterialTheme.typography
+    val dialogTypography = currentTypography.copy(
+        bodyLarge = currentTypography.bodyLarge.copy(
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 1.sp
+        ),
+        labelMedium = currentTypography.labelMedium.copy(
+            fontWeight = FontWeight.Bold,
+            letterSpacing = 0.2.em
+        )
+    )
+    MaterialTheme(
+        colorScheme = DarkDialogColorScheme,
+        typography = dialogTypography,
+        content = content
+    )
 }
